35d7f0c2b9e06d4de5602260bf2404fa12f00c4b
juandasimatupang94@gmail.com <pnaibaho64@gmail.com>
2023-04-18 15:14:09 +0700

    Komit Pertama

---
+++ str.php
@@ -0,0 +1,12 @@
+<?php
+echo strlen("Hello World!")."<br>"; //outputs 12
+
+echo str_word_count("Hello world!")."<br>"; //outputs 2
+
+echo strrev("Hello World!")."<br>"; //outputs !dlroW olleH
+
+echo strpos("Hello World!", "World")."<br>"; //outputs 6
+
+echo str_replace("World", "Pedro", "Hello World!")."<br>"; //outputs Hello Pedro
+?>
+
